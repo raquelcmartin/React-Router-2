a5d5552d1a6ca1f656a1c5acefbb3e7bc4157dcc
Raquel <raquel@CLAY-MARTINs-MacBook-Air.local>
2024-02-23 15:31:36 -0500

    done

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+    return (
+    <div className="Nav">
+        <h1>Home</h1>
+    </div>
+    )
+    }
---
+++ src/components/Red.jsx
@@ -0,0 +1,9 @@
+
+export default function Red() {
+    // why are we returning?
+    return ( 
+        <div className="red">
+            <h1>RED</h1>
+        </div>
+    );
+}
